4f4b7a2040e1f5dc06782790daaf3f3c4972975f
Serhii <serhii.ost@gmail.com>
2020-07-28 17:33:51 +0300

    правки

---
+++ app/src/main/java/lampa/test/tmdblib/view/fragments/FragmentMain.kt
@@ -0,0 +1,130 @@
+package lampa.test.tmdblib.fragments
+
+import android.os.AsyncTask
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import lampa.test.tmdblib.R
+import lampa.test.tmdblib.fragments.callback.CallBackFromFragmentToActivity
+import lampa.test.tmdblib.mvp.api.Results
+import lampa.test.tmdblib.view.recycler.RecyclerAdapter
+import lampa.test.tmdblib.view.recycler.callback.CallBackFromRecyclerToFragment
+
+class FragmentMain : Fragment(), CallBackFromRecyclerToFragment {
+
+    lateinit var recycler: RecyclerView
+    lateinit var linearLayoutManager: LinearLayoutManager
+    lateinit var gridLayoutManager: GridLayoutManager
+    lateinit var adapter: RecyclerAdapter
+
+    lateinit var allContent: ArrayList<Results>
+
+    lateinit var callBackFromFragmentToActivity: CallBackFromFragmentToActivity
+
+    var isDownload:Boolean = false
+
+    override fun onCreateView (inflater: LayoutInflater, container: ViewGroup?,
+                               savedInstanceState: Bundle?): View? {
+
+        val v: View = inflater.inflate(R.layout.fragment_main, null)
+        recycler = v.findViewById(R.id.recycler)
+
+        linearLayoutManager = LinearLayoutManager(v.context,LinearLayoutManager.VERTICAL,false)
+        gridLayoutManager = GridLayoutManager(context,3)
+        recycler.layoutManager = linearLayoutManager
+
+        recycler.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if (((recycler.layoutManager as LinearLayoutManager).findLastVisibleItemPosition() > recycler.adapter?.itemCount!!/2) &&
+                    !isDownload) {
+                    isDownload = true
+                    val runnableCode = object: Runnable {
+                        override fun run() {
+                            callBackFromFragmentToActivity.addMovieToList()
+                        }
+                    }
+                    runnableCode.run()
+                }
+            }
+        })
+
+        return v
+    }
+
+    fun setContent(res: List<Results>?){
+
+        val result_array = ArrayList(res)
+        allContent = result_array
+        adapter = RecyclerAdapter(result_array, 1, this as CallBackFromRecyclerToFragment)
+        recycler.adapter = adapter
+        AnimateRecycler()
+    }
+
+    inner class AddContent(res: List<Results>?): AsyncTask<Void, Void, Void>() {
+
+        val res: List<Results>?
+
+        init {
+            this.res = res
+        }
+
+        override fun doInBackground(vararg p0: Void?): Void? {
+            val result_array = ArrayList(res)
+            allContent.addAll(result_array)
+            return null
+        }
+
+        override fun onPostExecute(result: Void?) {
+
+            recycler.adapter?.notifyItemRangeInserted(recycler.adapter!!.itemCount,
+                                            recycler.adapter!!.itemCount+20)
+            isDownload = false
+            super.onPostExecute(result)
+        }
+    }
+
+    fun setLayoutManager(type: Int){
+
+        var oldScrollPos = (recycler.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+        when (type){
+            1 -> recycler.layoutManager = linearLayoutManager
+            2 -> recycler.layoutManager = gridLayoutManager
+        }
+        adapter.type = type
+        recycler.adapter = adapter
+        AnimateRecycler()
+        recycler.scrollToPosition(oldScrollPos)
+    }
+
+    fun AnimateRecycler(){
+
+        val controller = AnimationUtils.loadLayoutAnimation(context, R.anim.rec_in)
+        recycler.setLayoutAnimation(controller)
+        recycler.startLayoutAnimation()
+    }
+
+    override fun onActivityCreated(@Nullable savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        callBackFromFragmentToActivity = activity as CallBackFromFragmentToActivity
+    }
+
+    override fun onMovieClick(position: Int) {
+
+        callBackFromFragmentToActivity.openMovie(allContent.get(position))
+    }
+
+    override fun onFavoriteClick(position: Int) {
+        Toast.makeText(context,position.toString(), Toast.LENGTH_LONG).show()
+    }
+}
